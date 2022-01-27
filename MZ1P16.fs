189ffc3616b74b305b6d2047dcf2e89db2df8a14
cvjb <cvjb@users.noreply.github.com>
2022-01-27 23:23:22 +0100

    Minor formatting changes
    Tabs were not aligned after the upload.

--- MZ1P16.fs
+++ MZ1P16.fs
@@ -1,31 +1,31 @@
 #! /usr/bin/gforth
 \ 
-\ Projekt: Plottersteuerung für den MZ-1P16 (SHARP Electr. GmbH/SHARP Corp.)
+\ Projekt: Plottersteuerung fÃ¼r den MZ-1P16 (SHARP Electr. GmbH/SHARP Corp.)
 \ Name   : MZ1P16
 \ Zweck  : Plotterbefehle definieren.
-\ Autor  : Christian V. J. Brüssow <cvjb AT epost DOT de>
+\ Autor  : Christian V. J. BrÃ¼ssow
 \ Stand  : Sam 10 Jul 1999 19:18:39 MEST
 \ Notiz  : BETA
 \          !!! Bisher nur auf dem originalen MZ-821 unter MZ-FORTH getestet !!!
 \ 
 
-\ In ANS-/gforth unbekannte Wörter, die im Programm verwendet werden:
+\ In ANS-/gforth unbekannte WÃ¶rter, die im Programm verwendet werden:
 
 \ ASCII Codes von Zeichen generieren.
 : ASCII	( -- c )	\ Execute-Modus
-			( -- )	\ Compile-Modus: compiliert als Literal
-	32		\ mit ASCII 32 (Leerzeichen) als Trennzeuchen
-	WORD	\ separiere im Eingabestrom das nächste Wort als
-			\ Count-String. Übergib die Adresse des Count-Bytes.
+	( -- )		\ Compile-Modus: compiliert als Literal
+	32	\ mit ASCII 32 (Leerzeichen) als Trennzeuchen
+	WORD	\ separiere im Eingabestrom das nÃ¤chste Wort als
+		\ Count-String. Ãœbergib die Adresse des Count-Bytes.
 	1+ C@	\ greife auf das erste Zeichen im String zu
 	STATE @	\ und ... je nachdem ...
 	IF
 		POSTPONE LITERAL	\ compiliere als Literal,
-	ENDIF		\ oder übergib auf dem TOS.
+	ENDIF				\ oder Ã¼bergib auf dem TOS.
 ; IMMEDIATE	\ auch im Compiler-Modus aktiv sein.
 
-\ Abgewandeltes CR: nur Wagenrücklauf (Steuercode 0x0D)
-\ Das MZ-FORTH eigene CR' erhöht eine interne Variable OUT
+\ Abgewandeltes CR: nur WagenrÃ¼cklauf (Steuercode 0x0D)
+\ Das MZ-FORTH eigene CR' erhÃ¶ht eine interne Variable OUT
 \ um einen Wert, der sich aus modulo COL# (eine weitere,
 \ interne Variable) ergibt. Dies dient nur Formatierungs-
 \ zwecken. COL# ist in MZ-FORTH gleich der Anzahl Zeichen
@@ -36,17 +36,17 @@
 : R	( -- n )	R> DUP >R ;
 
 \ (LINE) liefert die Startadresse a der Zeile n1 des Screens n2, sowie
-\ deren Länge l (bei MZ-Forth immer 64). Die Zeilen zählen von 0 bis 15.
+\ deren LÃ¤nge l (bei MZ-Forth immer 64). Die Zeilen zÃ¤hlen von 0 bis 15.
 : (LINE)	( n1 n2 -- a l )
 	BUFFER		\ Anfangsadresse des Screens mit der Nummer n2
-	SWAP 64 *	\ n1 * 64 Bytes weiter fängt die Zeile n1 an
-	+				\ Anfangsadresse der Zeile n2 in n1
-	64 ;			\ Länge der Zeile (immer 64)
+	SWAP 64 *	\ n1 * 64 Bytes weiter fÃ¤ngt die Zeile n1 an
+	+		\ Anfangsadresse der Zeile n2 in n1
+	64 ;		\ LÃ¤nge der Zeile (immer 64)
 
-\ Dummy für die MZ-FORTH Variable PROTOCOL.
+\ Dummy fÃ¼r die MZ-FORTH Variable PROTOCOL.
 0 VARIABLE PROTOCOL
 
-\ Dummy für die MZ-FORTH Variable OUTDEV.
+\ Dummy fÃ¼r die MZ-FORTH Variable OUTDEV.
 0 VARIABLE OUTDEV
 
 
@@ -54,16 +54,16 @@
 VOCABULARY MZ1P16 IMMEDIATE
 MZ1P16 DEFINITIONS
 
-0 VARIABLE Farbe		\ enthält die aktuelle Farbnummer
+0 VARIABLE Farbe	\ enthÃ¤lt die aktuelle Farbnummer
 
 : +Farbe	( -- )	29 EMIT ;	\ Farbkopf um eine Position weiterdrehen
 
 : setzen ( b a -- )	\ neue Farbe einstellen
 	OVER SWAP 0 2DUP =
-	IF 2DROP DROP					\ neue Farbe = alte Farbe => nichts tun
+	IF 2DROP DROP			\ neue Farbe = alte Farbe => nichts tun
 	ELSE - DUP 0< IF 4 + ENDIF	\ Anzahl der 1/4-Drehungen des Farbkopfes
 	     0 DO +Farbe LOOP		\ Kopf auf neue Farbe drehen
-		  Farbe !					\ neue Farbe merken
+		  Farbe !		\ neue Farbe merken
 	ENDIF
 ;
 
@@ -80,19 +80,19 @@ MZ1P16 DEFINITIONS
 	Text 4 EMIT ;
 
 
-9 CONSTANT kleine		\ 80 Zeichen pro Zeile
+9 CONSTANT kleine	\ 80 Zeichen pro Zeile
 11 CONSTANT grosse	\ 40 Zeichen pro Zeile
 12 CONSTANT riesige	\ 26 Zeichen pro Zeile. Achtung: Steuercode ist
-							\ eigentlich gleich "11", der angegebene Wert dient
-							\ nur zur Unterscheidung zu "grosse". Die
-							\ entsprechende Auswertung findet in "Schriftart" statt.
+			\ eigentlich gleich "11", der angegebene Wert dient
+			\ nur zur Unterscheidung zu "grosse". Die
+			\ entsprechende Auswertung findet in "Schriftart" statt.
 : Schriftart	( u -- )
-	DUP 12 <> IF 9 EMIT 9 EMIT	\ klein u. gross brauchen diese "Vorgabe"
-				 ELSE DROP 11		\ riesig nicht, muß aber gleich 11 sein.
+	DUP 12 <> IF 9 EMIT 9 EMIT		\ klein u. gross brauchen diese "Vorgabe"
+				 ELSE DROP 11	\ riesig nicht, muÃŸ aber gleich 11 sein.
 				 THEN
 	EMIT ;
 
-: LF		( n -- )		\ Zeilenvorschübe, es sind auch negative Werte erlaubt!
+: LF		( n -- )		\ ZeilenvorschÃ¼be, es sind auch negative Werte erlaubt!
 	Text
 	DUP
 	0< IF 3
@@ -100,15 +100,15 @@ MZ1P16 DEFINITIONS
 		THEN SWAP
 	ABS 0 DO DUP EMIT LOOP DROP ;
 
-: FF		( u -- )		\ Seitenvorschub. Der Zeilenzähler wird auf 0 gesetzt.
+: FF		( u -- )		\ Seitenvorschub. Der ZeilenzÃ¤hler wird auf 0 gesetzt.
 	16 EMIT ;
 
 : L/P		( b -- )		\ Zeilen pro Seite (max. erlaubt sind 255)
 	9 EMIT 9 EMIT
-	ABS 255 AND >R		\ ungültige Werte unterdrücken
-	R 100 / [ ASCII 0 ] LITERAL + EMIT							\ 100er
-	R  10 / R 100 / 10 * - [ ASCII 0 ] LITERAL + EMIT		\ 10er
-	R R> 10 / 10 * - [ ASCII 0 ] LITERAL + EMIT				\ 1er
+	ABS 255 AND >R						\ ungÃ¼ltige Werte unterdrÃ¼cken
+	R 100 / [ ASCII 0 ] LITERAL + EMIT			\ 100er
+	R  10 / R 100 / 10 * - [ ASCII 0 ] LITERAL + EMIT	\ 10er
+	R R> 10 / 10 * - [ ASCII 0 ] LITERAL + EMIT		\ 1er
 	13 EMIT
 ;
 
@@ -128,21 +128,21 @@ ASCII ; VARIABLE (ENDE)	\ Eingabe-Endkennung
 
 : (.Text)	( u -- f )	\ Unterroutine zur Textausgabe mit ".Text"
 	16 0 DO	I OVER (LINE)
-				OVER C@ ENDE =				\ Endkennung erreicht?
+				OVER C@ ENDE =		\ Endkennung erreicht?
 				IF  2DROP DROP 0 LEAVE	\ Ja => Ausgabe beenden
-				ELSE	TAB @ SPACES		\ Tabulator
+				ELSE	TAB @ SPACES	\ Tabulator
 						-TRAILING TYPE CR	\ Zeile ausgeben
 				ENDIF
 		  LOOP ;
 
-\ Textausgabe über mehrere Screens, bis die Endkennung (Vorgabe: ";")
-\ oder eine ungültige Screennummer erreicht wird. Im letzteren Fall
+\ Textausgabe Ã¼ber mehrere Screens, bis die Endkennung (Vorgabe: ";")
+\ oder eine ungÃ¼ltige Screennummer erreicht wird. Im letzteren Fall
 \ erfolgt Abbruch der Ausgabe mit einer Fehlermeldung.
 : .Text:	( u -- )	\ u = Nummer des Screens
 	BEGIN
 		DUP (.Text)	\ Ausgabe des Screens
-	WHILE				\ Abbruch bei Flag = 0
-		1+				\ nächster Screen
+	WHILE			\ Abbruch bei Flag = 0
+		1+		\ nÃ¤chster Screen
 	REPEAT ;
 
 : LPT:	1 OUTDEV ! ;	\ Ausgabe nur auf dem Plotter
@@ -173,34 +173,34 @@ ASCII ; VARIABLE (ENDE)	\ Eingabe-Endkennung
 
 \ Kopfbewegungen
 : links	( u -- ) \ Plotterstift um u Zeichen nach links bewegen.
-					\ Ist der Plotterkopf am linken Seitenrand, wird diese
-					\ Aufforderung ignoriert.
-	ABS			\ Paranoia
+		\ Ist der Plotterkopf am linken Seitenrand, wird diese
+		\ Aufforderung ignoriert.
+	ABS	\ Paranoia
 	0 DO
 		14 EMIT	\ Steuercode 0x0E
 	LOOP ;
 
 \ Graphikkommandos
-\ NUR Für Graphikausgaben außerhalb von ".Graphik:".
+\ NUR FÃ¼r Graphikausgaben auÃŸerhalb von ".Graphik:".
 : GRAPHIK		\ Graphikmodus einschalten.
 	2 EMIT ;
 : TEXT			\ In Textmodus schalten; 40 Zeichen/Zeile.
 	ASCII A EMIT ;
 : LINIEN-TYP	( b -- )	\ 0 <= b <= 15
-					\ Linientyp: b =  0: durchgehende Linie, bis
-					\            b = 15: punktierte Linie
+				\ Linientyp: b =  0: durchgehende Linie, bis
+				\            b = 15: punktierte Linie
 	ASCII L EMIT . ;
-: URPSRUNG			\ Stift anheben und zur Ausgangsposition zurückbringen.
+: URPSRUNG			\ Stift anheben und zur Ausgangsposition zurÃ¼ckbringen.
 	ASCII H EMIT ;
 : URSPRUNG!		\ Aktuelle Stiftposition wird der Ursprung (x=0, y=0).
 	ASCII I EMIT ;
 : LINIE			( x y -- )	\ -999 <= x,y <= 999
 					\ Zeichnet eine Linie von der aktuellen Stiftpos. nach (x,y).
-	SWAP			\ x muß "D" bei der Ausgabe folgen.
+	SWAP				\ x muÃŸ "D" bei der Ausgabe folgen.
 	ASCII D EMIT . ASCII , EMIT . ;
 : RLINIE			( dx dy -- )	\ -999 <= dx,dy <= 999
-					\ wie "LINIE"; dx und dy sind aber relative Koordinaten.
-	SWAP			\ dx muß "J" bei der Ausgabe folgen.
+						\ wie "LINIE"; dx und dy sind aber relative Koordinaten.
+	SWAP			\ dx muÃŸ "J" bei der Ausgabe folgen.
 	ASCII J EMIT . ASCII , EMIT . ;
 : BEWEGEN		( x y -- )	\ -999 <= x,y <= 999
 					\ Bewegt den Stift an die Position (x,y).
@@ -208,16 +208,16 @@ ASCII ; VARIABLE (ENDE)	\ Eingabe-Endkennung
 	ASCII M EMIT . ASCII , EMIT . ;
 : RBEWEGEN		( dx dy -- )	\ -999 <= dx,dy <= 999
 					\ wie "BEWEGEN"; dx und dy sind aber relative Koordinaten.
-	SWAP			\ dx muß "R" bei der Ausgabe folgen.
+	SWAP				\ dx muÃŸ "R" bei der Ausgabe folgen.
 	ASCII R EMIT . ASCII , EMIT . ;
 : FARBE!			( b -- )	\ 0 <= c <= 3
-					\ Farbe wechseln, s.a. "+Farbe" und "setzen".
+						\ Farbe wechseln, s.a. "+Farbe" und "setzen".
 	DUP
 	ASCII C EMIT .
 	Farbe ! ;
-: SKALIERUNG	( b -- )	\ 0 <= b <= 63; Zeichengröße festlegen.
+: SKALIERUNG	( b -- )	\ 0 <= b <= 63; ZeichengrÃ¶ÃŸe festlegen.
 	ASCII S EMIT . ;
-: TEXT-DREHEN	( b -- )	\ 0 <= b <= 3; Textrichtung drehen (90° Schritte).
+: TEXT-DREHEN	( b -- )	\ 0 <= b <= 3; Textrichtung drehen (90Â° Schritte).
 	ASCII Q EMIT . ;
 : ZEICHEN	( c -- )	\ Plottet das Zeichen c.
 	ASCII P EMIT EMIT ;
